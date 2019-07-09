a00782ef77f8be372c2d9eedfef12bbe267f19ce
Heini Ahokas <ahokas.heini@gmail.com>
2019-07-09 16:26:41 +0100

    Add connection between graphcms and moltin produts

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,27 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.createResolvers = ({ createResolvers }) => {
+    const resolvers = {
+        GCMS_CodeEntry: {
+            mo_products: {
+                type: [`MoltinProduct`],
+                resolve: (source, args, context, info) => {
+                    return context.nodeModel.runQuery({
+                        query: {
+                            filter: {
+                                sku: {
+                                    in: source.music,
+                                },
+                            },
+                        },
+                        type: "MoltinProduct",
+                        firstOnly: false
+                    })
+                }
+            }
+        }
+        }
+        createResolvers(resolvers)
+}
